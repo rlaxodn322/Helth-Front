d6f465e60b77683d4e33222bf418900ed6a7c12e
taewoo <rlaxodn322@gmail.com>
2024-12-11 10:06:42 +0900

    test

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const backUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://localhost:3001'
-    : 'http://localhost:3001';
+    ? 'http://52.79.183.89'
+    : 'http://52.79.183.89';
 // export const backUrl =
 //   process.env.NODE_ENV === 'production' ? 'http://ele.firstcorea.com:3000' : 'http://3.39.135.179:3000';
